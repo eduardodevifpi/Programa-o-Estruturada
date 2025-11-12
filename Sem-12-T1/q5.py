30b0118dc1f0ca8cd3b86e69078cee80f694bf38
willianjonhy05 <willianjohny05@gmail.com>
2025-11-12 12:57:00 -0300

    Tentando a últiima

--- Sem-12-T1/q5.py
+++ Sem-12-T1/q5.py
@@ -1,19 +1,22 @@
-def main():
-    pop_inicial = int(input().strip())
-
-    populacao = float(pop_inicial)
+def calcular_populacao_dodo(populacao_inicial):
     ano = 1600
+    populacao_atual = populacao_inicial
+    limite = populacao_inicial * 0.1
 
-    while populacao >= 0.1 * pop_inicial:
-        nascimentos = populacao * 0.01
-        mortes = populacao * 0.06
-        populacao = populacao - mortes + nascimentos
+    while populacao_atual >= limite:
+        nascimentos = int(populacao_atual * 0.01)
+        mortes = int(populacao_atual * 0.06)
+        populacao_atual += nascimentos - mortes
 
-        
-        print(f"{ano},{int(nascimentos)},{int(mortes)},{int(populacao)}")
-
+        print(f"{ano},{nascimentos},{mortes},{int(populacao_atual)}")
         ano += 1
 
+def main():
+    try:
+        populacao_inicial = int(input())
+        calcular_populacao_dodo(populacao_inicial)
+    except ValueError:
+        print("Entrada inválida. Por favor, insira um número inteiro.")
 
 if __name__ == "__main__":
     main()
